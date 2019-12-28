04b466eb31b18c01c4080bab6a3a7d08f283d09c
Lodek <23224552+Lodek@users.noreply.github.com>
2019-12-28 15:55:37 -0300

    rubiks

--- merge-sort.hs
+++ merge-sort.hs
@@ -1,16 +1,15 @@
-
 -- Merge two lists in ascending order.
 -- Assume that both arguments are sorted lists.
 merge :: (Ord a) => [a] -> [a] -> [a]
-merge [] [] = []
+merge [] [] = [] 
 merge x [] = x
 merge [] y = y
 merge (x:xs) (y:ys)
-    | x < y = x : (merge xs (y:ys))
-    | y < x = y : (merge (x:xs) ys)
+   | x < y = x : (merge xs (y:ys))
+   | otherwise = y : (merge (x:xs) ys)
    
 -- Implementation of mergesort
 mergesort :: (Ord a) => [a] -> [a]
-mergesort [a] = a:[]
+mergesort [a] = a:[] -- if (lista.length == 1)
 mergesort xs = merge (mergesort (take half xs)) (mergesort (drop half xs))
     where half = (length xs) `div` 2
--- rubiks.hs
+++ rubiks.hs
@@ -1,6 +1,7 @@
-data Color = Red | Yellow | Green | Blue | Orange | White deriving (Show)
-data Face = Face Color Color Color Color deriving (Show)
-data Cube = Cube {front :: Face, right :: Face, up :: Face, back :: Face, left :: Face, down :: Face} deriving (Show)
+data Color = Red | Yellow | Green | Blue | Orange | White deriving (Show, Eq)
+data Orientation = Left | Right | Up | Down | Back | Front
+data Face = Face Color Color Color Color deriving (Show, Eq)
+data Cube = Cube {front :: Face, right :: Face, up :: Face, back :: Face, left :: Face, down :: Face} deriving (Show, Eq)
 
 --Face indexing function
 --Probably unncessary
@@ -11,34 +12,42 @@ sqr n (Face a b c d)
   | n == 2 = c
   | n == 3 = d
 
---Cube transformation functions
-cubePOVLeft :: Cube -> Cube
-cubePOVLeft (Cube {front=f, back=b, left=l, right=r, up=u, down=d}) = Cube {front=l, back=r, up=u, down=d, left=b, right=f}
 
-cubePOVRight :: Cube -> Cube
-cubePOVRight (Cube {front=f, back=b, left=l, right=r, up=u, down=d}) = Cube {front=r, back=l, up=u, down=d, left=f, right=b}
+reorientCube :: Orientaton -> Cube -> Cube
+reorientCube o cube@(Cube {front=f, back=b, left=l, right=r, up=u, down=d})
+  | o == Left = Cube {front=l, back=r, up=u, down=d, left=b, right=f}
+  | o == Right = Cube {front=r, back=l, up=u, down=d, left=f, right=b}
+  | o == Up = Cube {front=u, back=d, up=b, down=f, left=l, right=r}
+  | o == Down = Cube {front=d, back=u, up=f, down=b, left=l, right=r}
+  | o == Back = Cube {front=b, back=f, up=u, down=d, left=r, right=l}
+  | o == Front = cube
+  
+cubePOVLeft = reorientCube Left
 
-cubePOVBack :: Cube -> Cube
-cubePOVBack (Cube {front=f, back=b, left=l, right=r, up=u, down=d}) = Cube {front=b, back=f, up=u, down=d, left=r, right=l}
+cubePOVRight = reorientCube Right
 
-cubePOVUp :: Cube -> Cube
-cubePOVUp (Cube {front=f, back=b, left=l, right=r, up=u, down=d}) = Cube {front=u, back=d, up=b, down=f, left=l, right=r}
+cubePOVBack = reorientCube Back
 
-cubePOVDown :: Cube -> Cube
-cubePOVDown (Cube {front=f, back=b, left=l, right=r, up=u, down=d}) = Cube {front=d, back=u, up=f, down=b, left=l, right=r}
+cubePOVUp = reorientCube Up
+
+cubePOVDown = reorientCube Down
+
 
 frontRotation :: Cube -> Cube --clockwise 90 deg front rotation
-frontRotation (Cube {front=f, back=b, left=l, right=r, up=u, down=d}) = Cube {front=f, back=b, left=l', right=r', up=u', down=d'}
+frontRotation (Cube {front=f, back=b, left=l, right=r, up=u, down=d}) = Cube {front=f', back=b, left=l', right=r', up=u', down=d'}
   where u' = Face (sqr 0 u) (sqr 1 u) (sqr 3 l) (sqr 1 l)
         d' = Face (sqr 2 r) (sqr 0 r) (sqr 2 d) (sqr 3 d)
         l' = Face (sqr 0 l) (sqr 0 d) (sqr 2 l) (sqr 1 d)
         r' = Face (sqr 2 u) (sqr 1 r) (sqr 3 u) (sqr 3 r)
+        f' = Face (sqr 2 f) (sqr 0 f) (sqr 3 f) (sqr 1 f)
 
-leftRotation = frontRotation . cubePOVLeft
-rightRotation = frontRotation . cubePOVRight
-downRotation = frontRotation . cubePOVDown
-upRotation = frontRotation . cubePOVUp
-backRotation = frontRotation . cubePOVBack
+leftRotation = cubePOVRight. frontRotation . cubePOVLeft
+rightRotation = cubePOVLeft . frontRotation . cubePOVRight
+downRotation = cubePOVUp . frontRotation . cubePOVDown
+upRotation = cubePOVDown . frontRotation . cubePOVUp
+backRotation = cubePOVBack . frontRotation . cubePOVBack
+
+
 
 solvedFace :: Color -> Face
 solvedFace c = Face c c c c
