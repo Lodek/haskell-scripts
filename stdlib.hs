fe1b7e98abe65095806dc4a9e85783b71d1a1039
Lodek <23224552+Lodek@users.noreply.github.com>
2020-01-04 10:53:55 -0300

    filter func

---
+++ stdlib.hs
@@ -0,0 +1,8 @@
+--Redundant reimplementation of prelude functions
+
+
+filter: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+    | p x = x : filter' p xs
+    | otherwise = filter' p xs
